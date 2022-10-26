a857c905f5ecfbc648bef10a6889cfbb7da18ffd
dreyna <reyna_b@upeu.edu.pe>
2022-10-26 16:24:18 -0500

    v1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { RouterModule, ROUTES} from '@angular/router';
+import { PostsService } from './services/posts.service';
 
 @NgModule({
   declarations: [
+    
     AppComponent,
     HomeComponent,
     AboutComponent,
     NavbarComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+        BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
